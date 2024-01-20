af0b74b454e5855ebc2f8cd650b21dfa2cde7918
Noah <scott.613865@swl.k12.oh.us>
2024-01-20 14:36:12 -0500

    Added subsystems
    added Intake and shooter subsystems

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    //Remember to update ID  
+    private final CANSparkMax runMotor = new CANSparkMax(1, MotorType.kBrushless);
+
+
+    public void startMotor(){
+        runMotor.set(1);
+    }
+
+    public void stopMotor(){
+        runMotor.set(0);
+    }
+
+
+}
