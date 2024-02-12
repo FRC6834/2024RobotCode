3c312995dfdbb1fdec5105962ed826906ace32c7
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-02-12 09:42:07 -0500

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,28 +3,30 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SubsystemConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
 
     //Instead of directly passing in CAN ID, use variable created in Constants.java
     //Need to create 2 climber objects
     //Delete this comment when fixed - George
-    private final CANSparkMax runMotor = new CANSparkMax(15, MotorType.kBrushless);
-    private final CANSparkMax reverseMotor = new CANSparkMax(15, MotorType.kBrushless);
+    private final CANSparkMax climberLeft = new CANSparkMax(SubsystemConstants.kLeftClimber, MotorType.kBrushless);
+    private final CANSparkMax climberRight = new CANSparkMax(SubsystemConstants.kRightClimber, MotorType.kBrushless);
 
 
     //Need ability to make climber go up and down
     //Let's name the methods climbUp and climbDown or something similar
     //Delete this comment when fixed - George
     public void climbUp(){
-        runMotor.set(1);
+        climberLeft.set(1);
     }
 
     public void climbDown(){
-        runMotor.set(1);
+        climberRight.set(1);
     }
 
     public void stopMotor(){
-        runMotor.set(0);
+        climberRight.set(0);
+        climberLeft.set(0);
     }    
 }
