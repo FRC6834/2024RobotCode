12babdd398e655865be4570ed8603a28cb123c56
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-02-17 10:00:09 -0500

    comments

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -11,17 +11,18 @@ public class ShooterSubsystem extends SubsystemBase {
     private final CANSparkMax leftMotor = new CANSparkMax(SubsystemConstants.kLeftShooter, MotorType.kBrushless);
     private final CANSparkMax rightMotor = new CANSparkMax(SubsystemConstants.kRightShooter, MotorType.kBrushless);
 
-
+    //Shooter goes - 100%
+    //Right is reversed on purpose
     public void startMotor(){
         leftMotor.set(1);
         rightMotor.set(-1);
     }
-
+    //Stops shooter
     public void stopMotor(){
         leftMotor.set(0);
         rightMotor.set(0);
     }
-
+    //Shooter runs - 50%
     public void startMotorHalfSpeed(){
         leftMotor.set(.5);
         rightMotor.set(-.5);
