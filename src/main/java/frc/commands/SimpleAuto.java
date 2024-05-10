32282799ab4f2a95a8cf9842323169c0babec863
joemama514 <kleeh.612556@swl.k12.oh.us>
2024-05-10 01:49:18 -0400

    auto mode i made at 1 am that i cannot test

--- src/main/java/frc/commands/SimpleAuto.java
+++ src/main/java/frc/commands/SimpleAuto.java
@@ -1,14 +1,48 @@
 package frc.commands;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ConveyorSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.Constants.AutoConstants;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
 
 public class SimpleAuto extends SequentialCommandGroup{
-    public SimpleAuto(DriveSubsystem drive){
-      addCommands(
-        new DriveCommand(drive, 0, 0, 0, 0)
-      );  
+  /*since i can't test the code since im at my house rn i made a boolean for wether or not the robot will go for a second note 
+  or if it will just leave the starting area after shooting the preloaded note. if going for a 2nd note causes problems set this to false*/
+  boolean secondNote = true;
+    public SimpleAuto(DriveSubsystem drive, IntakeSubsystem intake, ConveyorSubsystem conveyor, ShooterSubsystem shooter){
+
+      //!!UNTESTED!!
+
+      if(secondNote==true){
+        addCommands(
+
+          //shoots preloaded note
+          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
+
+          //collects second note; drives backwards and uses intake at the same time
+          new ParallelCommandGroup(new DriveCommand(drive, -1.46,  1, 0, 0), new IntakeCommand(intake)),
+
+          //drive forward again and then shoots the collected note
+          new SequentialCommandGroup(new DriveCommand(drive, 1.46, 1, 0, 0), new ShooterCommand(shooter, conveyor)),
+
+          //leaves starting zone
+          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
+          new DriveCommand(drive, 1.5, -1, 0, 0))  //these two are a part of the same group i just set them on different lines for readability
+
+        );
+      }else{
+        addCommands(
+
+          //shoots preloaded note
+          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
+
+          //leaves starting zone
+          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
+          new DriveCommand(drive, 1.5, -1, 0, 0))  //these two are a part of the same group i just set them on different lines for readability
+        );
+      }
     }
-    
 }
