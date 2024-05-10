f523b1acf510938dcecf5fc87c282618b173f754
joemama514 <kleeh.612556@swl.k12.oh.us>
2024-05-10 17:59:34 -0400

    Update SimpleAuto.java

--- src/main/java/frc/commands/SimpleAuto.java
+++ src/main/java/frc/commands/SimpleAuto.java
@@ -9,13 +9,22 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 
 public class SimpleAuto extends SequentialCommandGroup{
-  /*since i can't test the code since im at my house rn i made a boolean for wether or not the robot will go for a second note 
-  or if it will just leave the starting area after shooting the preloaded note. if going for a 2nd note causes problems set this to false*/
+  /*whether or not the robot will go for a second note or if it will just leave the starting area after shooting the preloaded note. 
+  if going for a 2nd note causes problems set this to false*/
   boolean secondNote = true;
-    public SimpleAuto(DriveSubsystem drive, IntakeSubsystem intake, ConveyorSubsystem conveyor, ShooterSubsystem shooter){
 
-      //!!UNTESTED!!
+  /*what position the robot is in. will affect the path it takes so make sure to update this
+   * "left" is when the robot is to the right of the speaker, but it is to the left from the driver's POV.
+   * "center" is when the robot is in the middle.
+   * "right" is when the robot is to the left of the speaker, but it is to the right from the driver's POV.
+   * if the variable is set to anything else, the robot will simply drive out of the starting zone without shooting a note.*/
+  String startingPos = "center";
+  //RIGHT NOW THERE IS NO DIFFERENCE BETWEEN EACH POSITION BECAUSE I CANNOT ACCURATELY MAKE MEASUREMENTS WITHOUT BEING NEAR THE ROBOT
 
+  public SimpleAuto(DriveSubsystem drive, IntakeSubsystem intake, ConveyorSubsystem conveyor, ShooterSubsystem shooter){
+
+    //!!UNTESTED!!
+    if(startingPos=="center"){ //ROBOT IS IN THE CENTER OF THE SPEAKER AND WILL GO AFTER 2ND NOTE
       if(secondNote==true){
         addCommands(
 
@@ -30,10 +39,11 @@ public class SimpleAuto extends SequentialCommandGroup{
 
           //leaves starting zone
           new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
-          new DriveCommand(drive, 1.5, -1, 0, 0))  //these two are a part of the same group i just set them on different lines for readability
+          new DriveCommand(drive, 1.5, -1, 0, 0))  
+          //^^these two are a part of the same group i just set them on different lines for readability
 
         );
-      }else{
+      }else{ //ROBOT IS IN THE CENTER OF THE SPEAKER AND WILL GO NOT AFTER 2ND NOTE
         addCommands(
 
           //shoots preloaded note
@@ -41,8 +51,75 @@ public class SimpleAuto extends SequentialCommandGroup{
 
           //leaves starting zone
           new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
-          new DriveCommand(drive, 1.5, -1, 0, 0))  //these two are a part of the same group i just set them on different lines for readability
+          new DriveCommand(drive, 1.5, -1, 0, 0))  
+          //^^these two are a part of the same group i just set them on different lines for readability
+        );
+      }
+      }else if(startingPos=="left"){ //ROBOT IS ON LEFT SIDE OF THE SPEAKER (from driver pov) AND WILL GO AFTER 2ND NOTE
+      if(secondNote==true){
+        addCommands(
+
+          //shoots preloaded note
+          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
+
+          //collects second note; drives backwards and uses intake at the same time
+          new ParallelCommandGroup(new DriveCommand(drive, -1.46,  1, 0, 0), new IntakeCommand(intake)),
+
+          //drive forward again and then shoots the collected note
+          new SequentialCommandGroup(new DriveCommand(drive, 1.46, 1, 0, 0), new ShooterCommand(shooter, conveyor)),
+
+          //leaves starting zone
+          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
+          new DriveCommand(drive, 1.5, -1, 0, 0))
+          //^^these two are a part of the same group i just set them on different lines for readability
+
+        );
+      }else{ //ROBOT IS ON LEFT SIDE OF THE SPEAKER (from driver pov) AND WILL NOT GO AFTER 2ND NOTE
+        addCommands(
+
+          //shoots preloaded note
+          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
+
+          //leaves starting zone
+          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
+          new DriveCommand(drive, 1.5, -1, 0, 0))  
+          //^^these two are a part of the same group i just set them on different lines for readability
         );
       }
     }
+    else if(startingPos=="right"){ //ROBOT IS ON RIGHT SIDE OF THE SPEAKER (from driver pov) AND WILL GO AFTER 2ND NOTE
+      if(secondNote==true){
+        addCommands(
+
+          //shoots preloaded note
+          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
+
+          //collects second note; drives backwards and uses intake at the same time
+          new ParallelCommandGroup(new DriveCommand(drive, -1.46,  1, 0, 0), new IntakeCommand(intake)),
+
+          //drive forward again and then shoots the collected note
+          new SequentialCommandGroup(new DriveCommand(drive, 1.46, 1, 0, 0), new ShooterCommand(shooter, conveyor)),
+
+          //leaves starting zone
+          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
+          new DriveCommand(drive, 1.5, -1, 0, 0))  
+          //^^these two are a part of the same group i just set them on different lines for readability
+
+        );
+      }else{ //ROBOT IS ON RIGHT SIDE OF THE SPEAKER (from driver pov) AND WILL NOT GO AFTER 2ND NOTE
+        addCommands(
+
+          //shoots preloaded note
+          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
+
+          //leaves starting zone
+          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
+          new DriveCommand(drive, 1.5, -1, 0, 0))  
+          //^^these two are a part of the same group i just set them on different lines for readability
+        );
+      } //WILL NOT SHOOT NOTES; WILL ONLY DRIVE FORWARD (intended for use to gain leave points)
+    }else{
+      new DriveCommand(drive, 1.5, 1, 0, 0);
+    }
+  }
 }
