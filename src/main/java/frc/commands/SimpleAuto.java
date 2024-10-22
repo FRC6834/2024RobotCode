0ac0b845363761ccafa62340da93d661b4744fd3
Zephyros <zxphyros@gmail.com>
2024-10-22 09:53:49 -0400

    comment fixes

--- src/main/java/frc/commands/SimpleAuto.java
+++ src/main/java/frc/commands/SimpleAuto.java
@@ -9,14 +9,13 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 
 public class SimpleAuto extends SequentialCommandGroup{
-  /*whether or not the robot will go for a second note or if it will just leave the starting area after shooting the preloaded note. 
-  if going for a 2nd note causes problems set this to false*/
+  //whether or not the robot will go for a second note or if it will just leave the starting area after shooting the preloaded note
   final boolean SECOND_NOTE = true;
-  final  boolean FIRST_NOTE = true; //if false robot will just go for leave points
+  final boolean FIRST_NOTE = true; //if false robot will just go for leave points
 
   public SimpleAuto(DriveSubsystem drive, IntakeSubsystem intake, ConveyorSubsystem conveyor, ShooterSubsystem shooter){
-
-    //!!UNTESTED!!
+    
+    if(FIRST_NOTE){
       if(SECOND_NOTE){
         addCommands(
 
@@ -35,7 +34,7 @@ public class SimpleAuto extends SequentialCommandGroup{
           //^^these two are a part of the same group i just set them on different lines for readability
 
         );
-      }else{ //ROBOT IS ON RIGHT SIDE OF THE SPEAKER (from driver pov) AND WILL NOT GO AFTER 2ND NOTE
+      }else{ //ROBOT WILL NOT GO AFTER 2ND NOTE (SECOND_NOTE = false)
         addCommands(
 
           //shoots preloaded note
@@ -44,13 +43,11 @@ public class SimpleAuto extends SequentialCommandGroup{
           //leaves starting zone
           new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
           new DriveCommand(drive, 1.5, -1, 0, 0))  
-          //^^these two are a part of the same group i just set them on different lines for readability
+          //^^these two are a part of the same sequential command group i just set them on different lines for readability
         );
       } 
-    
-      //WILL NOT SHOOT NOTES; WILL ONLY DRIVE FORWARD (intended for use to gain leave points)
-    if(!FIRST_NOTE){
-      new DriveCommand(drive, 1.5, 1, 0, 0);
+    }else{ //FIRST_NOTE = false
+      new DriveCommand(drive, 1.5, 1, 0, 0); //WILL NOT SHOOT NOTES; WILL ONLY DRIVE FORWARD (intended for use to gain leave points)
     }
   }
 }
