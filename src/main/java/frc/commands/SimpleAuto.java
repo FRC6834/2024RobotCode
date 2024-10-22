673934723c3562898adf21b566c7336a8ed72930
Zephyros <zxphyros@gmail.com>
2024-10-22 08:29:52 -0400

    noo my terrible auto from 6 months ago

--- src/main/java/frc/commands/SimpleAuto.java
+++ src/main/java/frc/commands/SimpleAuto.java
@@ -11,84 +11,13 @@ import frc.robot.subsystems.ShooterSubsystem;
 public class SimpleAuto extends SequentialCommandGroup{
   /*whether or not the robot will go for a second note or if it will just leave the starting area after shooting the preloaded note. 
   if going for a 2nd note causes problems set this to false*/
-  boolean secondNote = true;
-
-  /*what position the robot is in. will affect the path it takes so make sure to update this
-   * "left" is when the robot is to the right of the speaker, but it is to the left from the driver's POV.
-   * "center" is when the robot is in the middle.
-   * "right" is when the robot is to the left of the speaker, but it is to the right from the driver's POV.
-   * if the variable is set to anything else, the robot will simply drive out of the starting zone without shooting a note.*/
-  String startingPos = "center";
-  //RIGHT NOW THERE IS NO DIFFERENCE BETWEEN EACH POSITION BECAUSE I CANNOT ACCURATELY MAKE MEASUREMENTS WITHOUT BEING NEAR THE ROBOT
+  final boolean SECOND_NOTE = true;
+  final  boolean FIRST_NOTE = true; //if false robot will just go for leave points
 
   public SimpleAuto(DriveSubsystem drive, IntakeSubsystem intake, ConveyorSubsystem conveyor, ShooterSubsystem shooter){
 
     //!!UNTESTED!!
-    if(startingPos=="center"){ //ROBOT IS IN THE CENTER OF THE SPEAKER AND WILL GO AFTER 2ND NOTE
-      if(secondNote==true){
-        addCommands(
-
-          //shoots preloaded note
-          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
-
-          //collects second note; drives backwards and uses intake at the same time
-          new ParallelCommandGroup(new DriveCommand(drive, -1.46,  1, 0, 0), new IntakeCommand(intake)),
-
-          //drive forward again and then shoots the collected note
-          new SequentialCommandGroup(new DriveCommand(drive, 1.46, 1, 0, 0), new ShooterCommand(shooter, conveyor)),
-
-          //leaves starting zone
-          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
-          new DriveCommand(drive, 1.5, -1, 0, 0))  
-          //^^these two are a part of the same group i just set them on different lines for readability
-
-        );
-      }else{ //ROBOT IS IN THE CENTER OF THE SPEAKER AND WILL GO NOT AFTER 2ND NOTE
-        addCommands(
-
-          //shoots preloaded note
-          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
-
-          //leaves starting zone
-          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
-          new DriveCommand(drive, 1.5, -1, 0, 0))  
-          //^^these two are a part of the same group i just set them on different lines for readability
-        );
-      }
-      }else if(startingPos=="left"){ //ROBOT IS ON LEFT SIDE OF THE SPEAKER (from driver pov) AND WILL GO AFTER 2ND NOTE
-      if(secondNote==true){
-        addCommands(
-
-          //shoots preloaded note
-          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
-
-          //collects second note; drives backwards and uses intake at the same time
-          new ParallelCommandGroup(new DriveCommand(drive, -1.46,  1, 0, 0), new IntakeCommand(intake)),
-
-          //drive forward again and then shoots the collected note
-          new SequentialCommandGroup(new DriveCommand(drive, 1.46, 1, 0, 0), new ShooterCommand(shooter, conveyor)),
-
-          //leaves starting zone
-          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
-          new DriveCommand(drive, 1.5, -1, 0, 0))
-          //^^these two are a part of the same group i just set them on different lines for readability
-
-        );
-      }else{ //ROBOT IS ON LEFT SIDE OF THE SPEAKER (from driver pov) AND WILL NOT GO AFTER 2ND NOTE
-        addCommands(
-
-          //shoots preloaded note
-          new ParallelCommandGroup(new ShooterCommand(shooter, conveyor)),
-
-          //leaves starting zone
-          new SequentialCommandGroup(new DriveCommand(drive, 2.9, 0, -1, 0), 
-          new DriveCommand(drive, 1.5, -1, 0, 0))  
-          //^^these two are a part of the same group i just set them on different lines for readability
-        );
-      }
-    }
-    else if(startingPos=="right"){ //ROBOT IS ON RIGHT SIDE OF THE SPEAKER (from driver pov) AND WILL GO AFTER 2ND NOTE
-      if(secondNote==true){
+      if(SECOND_NOTE){
         addCommands(
 
           //shoots preloaded note
@@ -117,8 +46,10 @@ public class SimpleAuto extends SequentialCommandGroup{
           new DriveCommand(drive, 1.5, -1, 0, 0))  
           //^^these two are a part of the same group i just set them on different lines for readability
         );
-      } //WILL NOT SHOOT NOTES; WILL ONLY DRIVE FORWARD (intended for use to gain leave points)
-    }else{
+      } 
+    
+      //WILL NOT SHOOT NOTES; WILL ONLY DRIVE FORWARD (intended for use to gain leave points)
+    if(!FIRST_NOTE){
       new DriveCommand(drive, 1.5, 1, 0, 0);
     }
   }
